406e9bf2a8e0ca8beb2e375f68384b21d4bb90ae
Darshan Pandhi <dapandhi@gmail.com>
2024-10-19 23:58:21 -0700

    feat: Setup next.js project + routes for laptops, employees

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div>
+      <h1>
+        This web app is created by the Analytics and Business Intelligence (ABI)
+        Team
+      </h1>
+      <p>Lead Developer: Darshan Pandhi</p>
+    </div>
+  );
+}
