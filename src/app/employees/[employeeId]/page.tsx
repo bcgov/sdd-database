12ccb0892e38cc36518150b4a82dac5c79d5f38d
Darshan Pandhi <dapandhi@gmail.com>
2024-10-20 15:20:09 -0700

    feat: Add custom 404 page for employees

--- src/app/employees/[employeeId]/page.tsx
+++ src/app/employees/[employeeId]/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+
 export default function Employees({
   params,
 }: {
@@ -5,5 +7,8 @@ export default function Employees({
     employeeId: string;
   };
 }) {
+  if (parseInt(params.employeeId) > 3) {
+    notFound();
+  }
   return <h1>Details for Employee {params.employeeId}</h1>;
 }
