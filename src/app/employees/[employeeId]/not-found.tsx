87bbaccaf046afbfdcba462b0b52fc9fc20892ce
Darshan Pandhi <dapandhi@gmail.com>
2024-10-20 15:20:09 -0700

    feat: Add custom 404 page for employees

---
+++ src/app/employees/[employeeId]/not-found.tsx
@@ -0,0 +1,13 @@
+export default function EmployeeNotFound() {
+  return (
+    <>
+      <h1>Error: Employee with this employee ID does not exist!</h1>
+      <h2>Please double-check the employee ID</h2>
+      <p>
+        If this is indeed unexpected, email Darshan Pandhi
+        (darshan.pandhi@gov.bc.ca) with a full page screenshot of this page as
+        well as mention the previous page you were on that led you to this page.{" "}
+      </p>
+    </>
+  );
+}
