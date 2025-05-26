3e5fca7fac1844e1fd1cf6d16915bed65f4fa499
Darshan Pandhi <dapandhi@gmail.com>
2025-05-26 00:19:23 -0700

    config: Trigger a new deployment

--- src/app/versions/page.tsx
+++ src/app/versions/page.tsx
@@ -9,6 +9,7 @@ export default function Updates() {
             version: "v0.0.7",
             notes: [
                 "Fixed bug allowing users to add duplicate IDIR employees by changing case",
+                "Test"
             ]
         },
         {
